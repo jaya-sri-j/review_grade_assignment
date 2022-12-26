a28b859fa81ef73a6af8070b44a4180bc639490c
jaya-sri-j <jayasrijagan28@gmail.com>
2022-12-26 10:38:49 +0530

    adding grade program.

---
+++ grade/js/main.js
@@ -0,0 +1,32 @@
+var mark=parseInt(prompt("enter mark :"))
+if(mark<=40)
+{
+	console.write("F");
+}
+else if(mark>40&&mark<=45)
+{
+	console.write("E");
+}
+else if(mark>45&&mark<=55)
+{
+	console.write("D");
+}
+else if(mark>55&&mark<=70)
+{
+	console.write("C");
+}
+else if(mark>70&&mark<=80)
+{
+	console.write("B");
+}
+else if(mark>80&&mark<=90)
+{
+	console.write("A");
+}
+else (mark>90)
+{
+	console.write("O")
+}
+
+
+
